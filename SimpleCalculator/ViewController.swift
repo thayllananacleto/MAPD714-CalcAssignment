15d92799c301fae0297b3fa5ad07196bc6f0a4d4
Thayllan Anacleto <thayllananacleto@Thayllans-MacBook-Pro.local>
2018-09-26 00:06:20 -0400

    More functionalities add, bug fixes, comments and reorganized some methods.

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -6,17 +6,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    // Enum used to identify the functions
+    //Enum used to identify the functions
     enum functions: Int {
         case dot = -1
         case clear = -2
         case plusMinus = -3
         case percentage = -4
-        case div = -5
-        case mult = -6
-        case sub = -7
-        case add = -8
-        case equals = -9
+    }
+    
+    //Enum used to identify the operators
+    enum operators: Int {
+        case div = -1
+        case mult = -2
+        case sub = -3
+        case add = -4
+        case equals = -5
     }
     
     //Instance Variables
@@ -31,7 +35,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultArea: UILabel!
     @IBOutlet weak var btnClear: UIButton!
     
-    //IBActions
+    //Method used when pressing any of the numbers or the "."
     @IBAction func onNumberButtonPress(_ sender: UIButton) {
         
         if(numberOnScreen == "0") {
@@ -46,15 +50,15 @@ class ViewController: UIViewController {
         }
         
         if(sender.tag == functions.dot.rawValue) {
-            if(!hasDecimal) {
-                numberOnScreen.append(".")
-                hasDecimal = true
+            if numberOnScreen != "0." {
+                if(!hasDecimal) {
+                    numberOnScreen.append(".")
+                }
             }
         }
         else if (lastInputWasOperator == true) {
             numberInMemory = numberOnScreen
             numberOnScreen = String(sender.tag)
-            lastInputWasOperator = false
         }
         else {
             numberOnScreen.append(String(sender.tag))
@@ -69,17 +73,21 @@ class ViewController: UIViewController {
         }
         
         keptPressingEquals = false
+        lastInputWasOperator = false
+        
+        //Variable to check if the number on screen is a decimal number
+        hasDecimal = (floor(Double(numberOnScreen)!) != Double(numberOnScreen)!)
         
         //Display the current result on the result area
         resultArea.text = numberOnScreen
         
     }
     
+    //Method used when pressing any of the functions
     @IBAction func onFunctionButtonPress(_ sender: UIButton) {
         
-        //Case structure to check which function button is being pressed
         switch sender.tag {
-        
+            
         //Function Clear
         case functions.clear.rawValue:
             if (numberOnScreen == "0" && numberInMemory == "0") {
@@ -96,7 +104,7 @@ class ViewController: UIViewController {
             else {
                 numberOnScreen = "0"
             }
-
+            
             btnClear.setTitle("AC", for: .normal)
             
         //Function Plus and Minus
@@ -111,85 +119,135 @@ class ViewController: UIViewController {
             
         //Function Percentage
         case functions.percentage.rawValue:
-            if (numberInMemory != "0" && numberOnScreen != "0" && lastOperatorPressed == functions.add.rawValue) {
+            if (numberInMemory != "0" && numberOnScreen != "0" && lastOperatorPressed == operators.add.rawValue) {
                 numberOnScreen = String(Double(numberInMemory)! * Double(numberOnScreen)! / Double(100))
             }
             else {
                 numberOnScreen = String(Double(numberOnScreen)! / (Double(100)))
             }
-            
-        //Operator Divide
-        case functions.div.rawValue:
-            //numberInMemory = numberOnScreen
-            lastInputWasOperator = true
-            lastOperatorPressed = functions.div.rawValue
-            
-        //Operator Multiply
-        case functions.mult.rawValue:
-            //numberInMemory = numberOnScreen
-            lastInputWasOperator = true
-            lastOperatorPressed = functions.mult.rawValue
-            
-        //Operator Subtract
-        case functions.sub.rawValue:
-            //numberInMemory = numberOnScreen
-            lastInputWasOperator = true
-            lastOperatorPressed = functions.sub.rawValue
-            
-        //Operator Add
-        case functions.add.rawValue:
-            //numberInMemory = numberOnScreen
-            lastInputWasOperator = true
-            lastOperatorPressed = functions.add.rawValue
-            
-        //Operator Equals
-        case functions.equals.rawValue:
-            
-            let nos: String = numberOnScreen
-            
-            //The operator Equals needs to consider the previous operator input, so I'm creating a case structure to make this verification
-            switch lastOperatorPressed {
-            
-            //Divide
-            case functions.div.rawValue:
-                if (keptPressingEquals) {
-                    numberOnScreen = String((Double(numberOnScreen)! / Double(numberInMemory)!))
-                }
-                else {
-                    numberOnScreen = String((Double(numberInMemory)! / Double(numberOnScreen)!))
-                }
-            
-            //Multiply
-            case functions.mult.rawValue:
-                numberOnScreen = String((Double(numberInMemory)! * Double(numberOnScreen)!))
-                
-            //Subtract
-            case functions.sub.rawValue:
-                if (keptPressingEquals) {
-                    numberOnScreen = String((Double(numberOnScreen)! - Double(numberInMemory)!))
-                }
-                else {
-                    numberOnScreen = String((Double(numberInMemory)! - Double(numberOnScreen)!))
-                }
-                
-            //Add
-            case functions.add.rawValue:
-                numberOnScreen = String((Double(numberInMemory)! + Double(numberOnScreen)!))
-            
-            //Apparently there has to be a default option for the switch case
-            default: break
-            }
-            
-            if (!keptPressingEquals) {
-                numberInMemory = nos
-            }
-            
-            keptPressingEquals = true
         
         //Apparently there has to be a default option for the switch case
         default: break
             
         }
+        
+        lastInputWasOperator = false
+        
+        //Variable to check if the number on screen is a decimal number
+        hasDecimal = (rint(Double(numberOnScreen)!) != Double(numberOnScreen)!)
+        
+        //Remove the decimal number if the result is a complete integer
+        if numberOnScreen.suffix(2) == ".0" {
+            numberOnScreen = String(numberOnScreen.prefix(numberOnScreen.count - 2))
+        }
+        
+        //Display the current result on the result area
+        if (numberOnScreen != "inf") {
+            resultArea.text = numberOnScreen
+        }
+        else {
+            resultArea.text = "Not a valid operation!"
+        }
+        
+    }
+    
+    //Method used when pressing any of the operators
+    @IBAction func onOperatorButtonPress(_ sender: UIButton) {
+        
+        //Case structure to check which function button is being pressed
+        switch sender.tag {
+            
+        //Operator Divide
+        case operators.div.rawValue:
+            if (numberInMemory != "0" && numberOnScreen != "0" && lastOperatorPressed != operators.equals.rawValue) {
+                equals()
+            }
+            
+            lastOperatorPressed = operators.div.rawValue
+            
+        //Operator Multiply
+        case operators.mult.rawValue:
+            if (numberInMemory != "0" && numberOnScreen != "0" && lastOperatorPressed != operators.equals.rawValue) {
+                equals()
+            }
+            
+            lastOperatorPressed = operators.mult.rawValue
+            
+        //Operator Subtract
+        case operators.sub.rawValue:
+            if (numberInMemory != "0" && numberOnScreen != "0" && lastOperatorPressed != operators.equals.rawValue) {
+                equals()
+            }
+            
+            lastOperatorPressed = operators.sub.rawValue
+            
+        //Operator Add
+        case operators.add.rawValue:
+            if (numberInMemory != "0" && numberOnScreen != "0" && lastOperatorPressed != operators.equals.rawValue) {
+                equals()
+            }
+            
+            lastOperatorPressed = operators.add.rawValue
+            
+        //Operator Equals
+        case operators.equals.rawValue:
+            equals()
+        
+        //Apparently there has to be a default option for the switch case
+        default: break
+            
+        }
+        
+        lastInputWasOperator = true
+        
+    }
+    
+    func equals() {
+        
+        let nos: String = numberOnScreen
+        
+        //The operator Equals needs to consider the previous operator input, so I'm creating a case structure to make this verification
+        switch lastOperatorPressed {
+            
+        //Divide
+        case operators.div.rawValue:
+            if (keptPressingEquals) {
+                numberOnScreen = String((Double(numberOnScreen)! / Double(numberInMemory)!))
+            }
+            else {
+                numberOnScreen = String((Double(numberInMemory)! / Double(numberOnScreen)!))
+            }
+            
+        //Multiply
+        case operators.mult.rawValue:
+            numberOnScreen = String((Double(numberInMemory)! * Double(numberOnScreen)!))
+            
+        //Subtract
+        case operators.sub.rawValue:
+            if (keptPressingEquals) {
+                numberOnScreen = String((Double(numberOnScreen)! - Double(numberInMemory)!))
+            }
+            else {
+                numberOnScreen = String((Double(numberInMemory)! - Double(numberOnScreen)!))
+            }
+            
+        //Add
+        case operators.add.rawValue:
+            numberOnScreen = String((Double(numberInMemory)! + Double(numberOnScreen)!))
+            
+        //Apparently there has to be a default option for the switch case
+        default: break
+        }
+        
+        if (!keptPressingEquals) {
+            numberInMemory = nos
+        }
+        
+        keptPressingEquals = true
+        lastInputWasOperator = true
+        
+        //Variable to check if the number on screen is a decimal number
+        hasDecimal = (rint(Double(numberOnScreen)!) != Double(numberOnScreen)!)
         
         //Remove the decimal number if the result is a complete integer
         if numberOnScreen.suffix(2) == ".0" {
